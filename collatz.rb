3af76ee124fff063f50487112e145cd47460cffc
Amber McGregor <amber@vqhomes.com>
2019-06-20 20:31:59 +0000

    Added findLongestSeqLength

--- collatz.rb
+++ collatz.rb
@@ -1,4 +1,6 @@
-def collatzSequence(num)
+$collArray = Array.new
+
+def getCollatzSequence(num)
     array = Array.new
     array << num
 
@@ -13,19 +15,59 @@ def collatzSequence(num)
     return array
 end
 
+def getCollatzSequenceLength(num)
+    seqLength = 0
+    startNum = num
+
+    while num > 1
+        if startNum > num
+            return seqLength + $collArray[num]
+        end
+
+        if num.even?
+            num = num/2
+        else
+            num = 3 * num +1
+        end
+        seqLength = seqLength + 1
+    end
+    return seqLength
+end
+
 def findLongestSeq(num)
     i = 1
     startSeq = 0
     maxLength = 0
+    
 
     while ( i <= num)
-        array = collatzSequence(i)
+        array = getCollatzSequence(i)
         cLength = array.length
+        if (cLength > maxLength)
+            maxLength = cLength
+            startSeq = i
+        end
+        $collArray[i] = cLength
+        i = i + 1
+    end
+   
+    return startSeq, maxLength
+end
+
+
+def findLongestSeqLength(num)
+    i = 1
+    startSeq = 0
+    maxLength = 0
+    
+    while ( i <= num)
+        cLength = getCollatzSequenceLength(i)
         
         if (cLength > maxLength)
             maxLength = cLength
             startSeq = i
         end
+        $collArray[i] = cLength
         i = i + 1
     end
    
@@ -33,3 +75,11 @@ def findLongestSeq(num)
 end
 
 puts findLongestSeq(1000000)
+puts findLongestSeqLength(1000000)
+
+ require 'benchmark'
+ num = 10000
+ Benchmark.bm do |x|
+   x.report("findLongestSeq") { findLongestSeq(num) }
+   x.report("findLongestSeqLength")  { findLongestSeqLength(num)  }
+ end
